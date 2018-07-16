c59e0f69a1c2f0c18c24674fd3a97a3d99a1a139
Shahid <shahid.sarker@gmail.com>
2018-07-16 10:51:50 -0400

    set up html, css, and scss files

---
+++ main.js
@@ -0,0 +1,23 @@
+class Pokemon {
+    constructor(hp, attack, defense, ability) {
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.ability = [];
+    }
+}
+
+class Trainer {
+    constructor() {}
+    all() {
+        return pokemonArray;
+    }
+    get(name) {
+        return trainerPokemon.name;
+    }
+    add(pokemonObject) {
+
+    }
+
+
+}
